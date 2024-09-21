19bbdeb432f13bee6c7aed6a5e2caf731c46225e
David de Hilster <dehilster@gmail.com>
2024-09-21 07:42:04 -0400

    Delete dist directory

--- dist/globals.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.treeFile = exports.visualText = void 0;
-const visualText_1 = require("./visualText");
-const treeFile_1 = require("./treeFile");
-exports.visualText = new visualText_1.VisualText();
-exports.treeFile = new treeFile_1.TreeFile();
